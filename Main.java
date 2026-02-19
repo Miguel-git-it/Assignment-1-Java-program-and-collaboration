26b207d079f04945498c66555a288e9e4224a099
Miguel-git-it <miguelsaucedo2345@gmail.com>
2026-02-19 09:41:15 -0600

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+
+public class Main {
+
+
+    public static void main(String[] args) {
+
+        // Required parallel arrays
+        String[] itemNames = new String[10];
+        double[] itemPrices = new double[10];
+        int[] itemStocks = new int[10];
+
+
+
+        System.out.println("Grocery Manager initialized.");
+        System.out.println("Feature branches will implement functionality.");
+    }
+
+
+    public static void printInventory(String[] names, double[] prices, int[] stocks) {
+
+    }
+
+
+    public static void restockItem(String[] names, int[] stocks, String target, int amount) {
+
+    }
+}
