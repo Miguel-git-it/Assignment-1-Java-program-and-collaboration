dcb675fc4878eab323549fff46403703011b2a35
Carter Atnip <80222483+CarterAtnip@users.noreply.github.com>
2026-02-20 14:10:34 -0600

    Added functionality to restockItem method

--- Main.java
+++ Main.java
@@ -55,6 +55,16 @@ public class Main {
 
 
     public static void restockItem(String[] names, int[] stocks, String target, int amount) {
+        boolean found = false;
 
+        for (int i = 0; i < names.length; i++) {
+            if (names[i].equals(target)) {
+                stocks[i] += amount;
+                found = true;
+                break;
+            }
+        }
+
+        if (!found) {System.out.println("Item not found.");}
     }
 }
